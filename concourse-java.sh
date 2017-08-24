3f613325914cdbb9d27398354f61d645a5c13eda
Phillip Webb <pwebb@pivotal.io>
2017-08-24 11:58:33 -0700

    Add setup_symlinks function

--- concourse-java.sh
+++ concourse-java.sh
@@ -10,3 +10,9 @@
 #     source <folder>/concourse-java.sh
 #
 # ============================================================================
+
+# Setup Maven and Gradle symlinks for caching
+setup_symlinks() {
+	[[ -d $PWD/maven && ! -d $HOME/.m2 ]] && ln -s "$PWD/maven" "$HOME/.m2"
+	[[ -d $PWD/gradle && ! -d $HOME/.gradle ]] && ln -s "$PWD/gradle" "$HOME/.gradle"
+}
